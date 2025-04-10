4e47daff1205d88299d0f87f996769aa8beb48a4
yunjikimm <yunji.kmm@gmail.com>
2025-04-10 20:15:36 +0900

    [feature] setup coredata habit model

--- HaveAGoodHabit/HaveAGoodHabit/App/HaveAGoodHabitApp.swift
+++ HaveAGoodHabit/HaveAGoodHabit/App/HaveAGoodHabitApp.swift
@@ -12,6 +12,7 @@ struct HaveAGoodHabitApp: App {
     var body: some Scene {
         WindowGroup {
             HabitListView()
+                .environment(\.managedObjectContext, PersistenceController.shared.viewContext)
         }
     }
 }
